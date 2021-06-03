903cd19fcf620bfb7b89abe27b35db9d0e20dbbe
nicod-man <nico.mantovani95@gmail.com>
2021-06-03 15:45:38 +0200

    added pd to docker-compose.yml

--- docker/start.bash
+++ docker/start.bash
@@ -8,9 +8,9 @@ export SPD_DIR=`pwd | gawk '{ print gensub(/\/docker/, "", 1) }'`
 
 echo "Running all services..."
 
-docker-compose -f $DCF pull $XSERVER fer facedetection
-docker-compose -f $DCF up -d $XSERVER fer facedetection
+docker-compose -f $DCF pull $XSERVER fer facedetection persondetection
+docker-compose -f $DCF up -d $XSERVER fer facedetection persondetection
 
-sleep 5
+sleep 3
 docker ps
 sleep 3
